3c8fb836ef22727eb5f374d5d5a36f3d40a75df3
SAC-CS112-Romero-Joel <Joelasap101@gmail.com>
2015-10-28 20:08:18 -0700

    update

--- src/randomnumbertest/RandomNumber.java
+++ src/randomnumbertest/RandomNumber.java
@@ -4,46 +4,16 @@ import java.util.Scanner;
 
 public class RandomNumber {
 
-	public void displaymessage() {
-		Scanner input = new Scanner(System.in);	
+	public static int GetaNumber() 
+	{
 		
-		System.out.println("Odd of Even Game");
-		
-		int i = 1;
-		int compNum;
-		
-		while (i <10 )
-		{
 		int gamenum = 1 + (int) (Math.random() *2);
-		compNum = gamenum;
-	System.out.print("Guess even or odd 1 for odd and 2 for even you get 10x turns");
-	int ans = input.nextInt();
-
-	if (ans<1 || ans >2)
-	 {
-	System.out.println("ERROR INVALID INPUT loss 1 turn");
-	 }
-
-	else if (ans ==1 && gamenum ==1 )
-	{System.out.println("Odd correct");
-
+		return gamenum;
 	}
-	else if (ans ==1 && gamenum ==2)
-	{System.out.println("I got odd you got even");
-
+	public int GetaNumber (int high)
+	{
+		int gamenum = 1 + (int) (Math.random() *high);
+		return gamenum;
 	}
-	else if (ans==2 && gamenum ==2)
-	{System.out.println("we both got even");
-
-	}
-	else if (ans==2 && gamenum == 1)
-	{System.out.println("I got odd you got even");
-
-	}
-	i++;
-	}
-		
-	}
-	
 	
 }
--- src/randomnumbertest/test.java
+++ src/randomnumbertest/test.java
@@ -1,12 +1,47 @@
 package randomnumbertest;
 
+import java.util.Scanner;
+
 public class test {
 
 	public static void main(String[] args) {
-		RandomNumber myRandomNumber = new RandomNumber();
-		
-		myRandomNumber.displaymessage();
 
+Scanner input = new Scanner(System.in);	
+		
+		System.out.println("Odd of Even Game");
+		
+		int i = 1;
+		int compNum= randomnumbertest.RandomNumber.GetaNumber();
+		
+		while (i <10 )
+		{
+		
+	System.out.print("Guess even or odd 1 for odd and 2 for even you get 10x turns");
+	int ans = input.nextInt();
+
+	if (ans<1 || ans >2)
+	 {
+	System.out.println("ERROR INVALID INPUT loss 1 turn");
+	 }
+
+	else if (ans ==1 && compNum ==1 )
+	{System.out.println("Odd correct");
+
+	}
+	else if (ans ==1 && compNum ==2)
+	{System.out.println("I got odd you got even");
+
+	}
+	else if (ans==2 && compNum ==2)
+	{System.out.println("we both got even");
+
+	}
+	else if (ans==2 && compNum == 1)
+	{System.out.println("I got odd you got even");
+
+	}
+	i++;
+	}
 	}
 
 }
